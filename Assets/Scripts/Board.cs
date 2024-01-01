92809150a0cb898e35709157ba22d9e3a3e6decd
Ho Anh Minh <yamchahuyvs@gmail.com>
2024-01-01 21:56:18 +0700

    [9. GamePiece]

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,6 +1,7 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
 
 public class Board : MonoBehaviour
@@ -8,13 +9,19 @@ public class Board : MonoBehaviour
     public int width;
     public int height;
     public int borderSize;
+
     public GameObject tilePrefab;
+    public GameObject[] gamePiecePrefabs;
+
     private Tile[,] m_AllTiles;
+    GameObject[,] m_AllGamePieces;
     void Start()
     {
         m_AllTiles = new Tile[width, height];
+        m_AllGamePieces = new GameObject[width, height];
         SetupTiles();
         SetupCamera();
+        FillRandom();
     }
     private void SetupTiles()
     {
@@ -37,5 +44,27 @@ public class Board : MonoBehaviour
         float verticalSize = height*1f/2 +borderSize;
         float horizontalSize = (width*1f/2 + borderSize) / aspectRatio;
         Camera.main.orthographicSize = verticalSize>horizontalSize?verticalSize:horizontalSize;
+    }
+    private GameObject GetRandomPiece()
+    {
+        int randomIndx = Random.Range(0,gamePiecePrefabs.Length);
+        return gamePiecePrefabs[randomIndx];
+    }
+    private void PlaceGamePiece(GamePiece gamePiece,int x,int y)
+    {
+        gamePiece.transform.position = new Vector3(x, y, 0);
+        gamePiece.transform.rotation = Quaternion.identity;
+        gamePiece.SetCoord(x, y);
+    }
+    private void FillRandom()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                GameObject randomPiece = Instantiate(GetRandomPiece(), Vector3.zero, Quaternion.identity);
+                PlaceGamePiece(randomPiece.GetComponent<GamePiece>(),i,j);
+            }
+        }
     }
 }
